09f8d6129f9b9e6a0ed6bcb73f3e822963d4ba1e
tlagmlwns <simh4jun@gmail.com>
2023-10-10 12:00:46 +0900

    activity_7 임시저장

--- app/src/main/java/com/example/myapplication/MainActivity7.java
+++ app/src/main/java/com/example/myapplication/MainActivity7.java
@@ -2,13 +2,32 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity7 extends AppCompatActivity {
+    private ImageButton ulc_Mypage;
+    private ImageButton ulc_Back;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main7);
+        setContentView(R.layout.activity_7_);
+        ulc_Back = findViewById(R.id.ibtn_ulcBack);
+        ulc_Mypage = findViewById(R.id.ibtn_ulcmypage);
+        ulc_Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { finish();}
+        });
+        ulc_Mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ulcmypage=new Intent(getApplicationContext(), MainActivity6.class);
+                startActivity(ulcmypage);
+            }
+        });
     }
 }
