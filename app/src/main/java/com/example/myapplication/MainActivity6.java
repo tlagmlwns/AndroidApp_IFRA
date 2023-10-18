70148ffef0852607ab6ef948dcfe19b297f13591
tlagmlwns <simh4jun@gmail.com>
2023-10-18 12:32:14 +0900

    임시저장

--- app/src/main/java/com/example/myapplication/MainActivity6.java
+++ app/src/main/java/com/example/myapplication/MainActivity6.java
@@ -11,9 +11,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,15 +37,16 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity6 extends AppCompatActivity {
-
+    Single_ton_data mydata = Single_ton_data.getInstance();
     ImageButton ibtn_logout;
     ImageButton ibtn_Back;
     Button edit_Mypageinfo;
     TabItem Tab_myinfo;
+    Spinner S_Grp;
+    String E_Grp;
     TextView tv_name, tv_id, tv_pw, tv_grp, tv_pnum;
-    String name, id, pw, grp, pnum;
-    String server_Result;
-
+    EditText E_Name, E_ID, E_PW, E_PNum;
+    String name, id, pw, grp, grp_num, pnum, old_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +56,9 @@ public class MainActivity6 extends AppCompatActivity {
 
         ibtn_logout = findViewById(R.id.ibtn_logout);
         ibtn_Back = findViewById(R.id.ibtn_ulcBack);
-        //TableLayout tabLayout = findViewById(R.id.tabs);
         edit_Mypageinfo = findViewById(R.id.btn_mypage_edit);
         Tab_myinfo = findViewById(R.id.Tab_myinfo);
+        showMyInfo();
         ibtn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,24 +87,82 @@ public class MainActivity6 extends AppCompatActivity {
         edit_Mypageinfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showEditableDialog(this::onClick, name, id, pw, grp, pnum);
+                showEditableDialog(this, name, id, pw, grp, pnum,
+                        new MainActivity6.OnValuesEditedListener() {
+                            @Override
+                            public void onValues_Edited(String editedName, String editedId, String editedPW, String editedGroup, String editedPnum) {
+                                // 사용자가 수정한 값을 사용
+                                // 이곳에서 수정된 값을 사용하거나 처리
+                                // editedName: 수정된 이름, editedIdNumber: 수정된 주민번호, editedGender: 수정된 성별, editedInstitution: 수정된 인증기관
+                                // 예시로 수정된 이름을 Toast 메시지로 출력하는 예시:
+                                Toast.makeText(MainActivity6.this, "수정완료 ", Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
     }
+    public void showMyInfo(){
+        tv_name = findViewById(R.id.tv_mypage_rName);
+        tv_id = findViewById(R.id.tv_mypage_rID);
+        tv_pw = findViewById(R.id.tv_mypage_rPW);
+        tv_grp = findViewById(R.id.tv_mypage_rGroup);
+        tv_pnum = findViewById(R.id.tv_mypage_rPNum);
+
+        name = mydata.getUser_name();
+        id = mydata.getUser_id();
+        if (old_id == id) {old_id = id;}
+        pw = mydata.getUser_pass();
+        grp = mydata.getUser_group();
+        if (grp == "Class C") { grp_num = "1";}
+        else if (grp == "Class Java") { grp_num = "2";}
+        pnum = mydata.getUser_phoneNum();
+        if (pnum.length() == 11) {
+            pnum = pnum.substring(0, 3) + "-" + pnum.substring(3, 7) + "-" + pnum.substring(7);
+        }
+
+        tv_name.setText(name);
+        tv_id.setText(id);
+        tv_pw.setText(pw);
+        tv_grp.setText(grp);
+        tv_pnum.setText(pnum);
+
+        String originalPw = tv_pw.getText().toString(); // tv_pw의 현재 문자열을 가져옴
+        int length = originalPw.length();
+        String maskedText = new String(new char[length]).replace("\0", "*"); // 길이만큼 '*'로 채운 문자열 생성
+        tv_pw.setText(maskedText);
+    }
     public void showEditableDialog(View.OnClickListener context, String edName, String edID, //v2
-                                   String edPW, String edGroup, String edPNum) {
+                                   String edPW, String edGroup, String edPNum, final MainActivity6.OnValuesEditedListener listener) {
         LayoutInflater inflater = LayoutInflater.from(this);
         View viewV1 = inflater.inflate(R.layout.activity_6_x1_mypageedit, null);
-        EditText E_Name = viewV1.findViewById(R.id.etv_mypage_ename);
-        EditText E_ID = viewV1.findViewById(R.id.etv_mypage_eID);
-        EditText E_PW = viewV1.findViewById(R.id.etv_mypage_ePW);
-        EditText E_Grp = viewV1.findViewById(R.id.etv_mypage_eGroup);
-        EditText E_PNum = viewV1.findViewById(R.id.etv_mypage_ePNum);
-
+        E_Name = viewV1.findViewById(R.id.etv_mypage_ename);
         E_Name.setText(edName);
+
+        E_ID = viewV1.findViewById(R.id.etv_mypage_eID);
         E_ID.setText(edID);
+
+        E_PW = viewV1.findViewById(R.id.etv_mypage_ePW);
         E_PW.setText(edPW);
-        E_Grp.setText(edGroup);
+
+        S_Grp = viewV1.findViewById(R.id.sp_mypage_grp);
+        String[] options = {"Class C", "Class Java"}; //1, 2
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, options);
+        S_Grp.setAdapter(adapter);
+        S_Grp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // 사용자가 선택한 항목의 텍스트 가져오기
+                E_Grp = parentView.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), "선택: " + E_Grp, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // 사용자가 아무것도 선택하지 않았을 때의 동작 정의
+                Toast.makeText(getApplicationContext(), "항목을 선택하세요.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        E_PNum = viewV1.findViewById(R.id.etv_mypage_ePNum);
         E_PNum.setText(edPNum);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -109,12 +171,24 @@ public class MainActivity6 extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         // 사용자가 수정한 값을 가져옵니다.
-                        //name = etName.getText().toString();
+                        mydata.setUser_name(E_Name.getText().toString());
+                        mydata.setUser_id(E_ID.getText().toString());
+                        mydata.setUser_pass(E_PW.getText().toString());
+                        mydata.setUser_group(E_Grp);
+                        mydata.setUser_phoneNum(E_PNum.getText().toString());
 
-                        // 리스너를 통해 수정된 값을 전달합니다.
-                        //if (listener != null) {
-                        //    listener.onValues_Edited(name, id, pw, grp, pnum);
-                        //}
+                        name = mydata.getUser_name();
+                        id = mydata.getUser_id();
+                        pw = mydata.getUser_pass();
+                        grp = mydata.getUser_group();
+                        pnum = mydata.getUser_phoneNum();
+
+                        if (listener != null) {
+                            listener.onValues_Edited(name, id, pw, grp, pnum);
+                            finish();
+                            startActivity(getIntent());
+                            //Send2_edinfo(); // 수정한 정보 업데이트
+                        }
 
                         Confirmation();
                     }
@@ -138,70 +212,20 @@ public class MainActivity6 extends AppCompatActivity {
         // 안내문자서 취소 클릭시
         Toast.makeText(getApplicationContext(), "취소하였습니다.", Toast.LENGTH_SHORT).show();
     }
-    public void Find_info() { // 서버로 보내기
+    public void Send2_edinfo() { // 서버로 보내기(수정시)
 
         RequestBody requestBody = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
-                .addFormDataPart("name",name)
-                .addFormDataPart("id",id)
-                .build();
-
-        Request request = new Request.Builder()
-                .url("http://15.164.120.162:5000//Sign_up") // 여기 sign_up 말고 딴거 넣어야됨
-                .post(requestBody)
-                .build();
-
-        OkHttpClient client = new OkHttpClient();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    String responseBody = response.body().string();
-
-                    try {
-                        JSONObject json = new JSONObject(responseBody);
-
-                        // 여기에서 JSON 데이터를 처리하고 원하는 정보 추출
-                        String status = json.getString("status");
-                        String result = json.getString("result");
-
-                        Log.d("JSON Response: ", "status: " + status);
-                        Log.d("JSON Response: ", "result: " + result);
-                        server_Result = result;
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                //아니 서버서 받은 결과 필터링 해야되나
-                                //showInfoDialog(MainActivity3.this,ocr_result);
-                            }
-                        });
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    // 응답이 성공적이지 않은 경우에 대한 처리
-                    Log.e("Response Error", "Response Code: " + response.code());
-                }
-            }
-        });
-    }
-    public void Send2_edinfo(File file) { // 서버로 보내기(수정시)
-
-        RequestBody requestBody = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
+                .addFormDataPart("old_id",old_id)//이전 아이디
                 .addFormDataPart("name",name) //이름
                 .addFormDataPart("id",id) //아이디
                 .addFormDataPart("pw",pw) //비번
-                .addFormDataPart("group",grp) //그룹
+                .addFormDataPart("group",grp_num) //그룹 번호 1:Class C, 2:Class Java
                 .addFormDataPart("pnum",pnum) //폰넘
                 .build();
 
         Request request = new Request.Builder()
-                .url("http://15.164.120.162:5000//Sign_up") // 여기 sign_up 말고 딴거 넣어야됨
+                .url("http://15.164.120.162:5000//alter") // 여기 sign_up 말고 딴거 넣어야됨
                 .post(requestBody)
                 .build();
 
