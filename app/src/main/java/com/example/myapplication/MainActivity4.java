7545f54f8f7cb7a554adde9391f79685a07dee5d
tlagmlwns <simh4jun@gmail.com>
2023-10-18 14:35:25 +0900

    환영인사 추가

--- app/src/main/java/com/example/myapplication/MainActivity4.java
+++ app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -17,11 +17,12 @@ import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity4 extends AppCompatActivity {
+    Single_ton_data mydata = Single_ton_data.getInstance();
     ImageButton um_Mypage; //um_Mypage : User Main -> Mypage
     Button um_Logcheck; //um_Logcheck : User Main -> Logcheck
     Button Inquiry;
     CountDownTimer countDownTimer;
-    TextView timeTextView;
+    TextView Welcome, timeTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +32,7 @@ public class MainActivity4 extends AppCompatActivity {
         um_Mypage = findViewById(R.id.ibtn_mypage);
         um_Logcheck = findViewById(R.id.btn_lcuser);
         Inquiry = findViewById(R.id.btn_Inquiry);
+        Welcome = findViewById(R.id.tv_Welcome);
         timeTextView = findViewById(R.id.tv_Time);
         um_Mypage.setOnClickListener(new View.OnClickListener() { //마이페이지 바로가기
             @Override
@@ -58,7 +60,7 @@ public class MainActivity4 extends AppCompatActivity {
                 dialog.show();
             }
         });
-
+        Welcome.setText(mydata.getUser_name()+"님 환영합니다!"); //welcome에 님 환영합니다 넣어야됨
         countDownTimer = new CountDownTimer(Long.MAX_VALUE, 1000) { //메인페이지 현재날짜띄우기
             public void onTick(long millisUntilFinished) {
                 updateTime();
