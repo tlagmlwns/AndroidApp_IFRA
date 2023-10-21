47292c861bca0dcc2e0268313252b806d73e26b7
tlagmlwns <simh4jun@gmail.com>
2023-10-21 17:42:44 +0900

    관리페이지 임시저장

--- app/src/main/java/com/example/myapplication/MainActivity8.java
+++ app/src/main/java/com/example/myapplication/MainActivity8.java
@@ -3,14 +3,22 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity8 extends AppCompatActivity {
-    ImageButton mpBack;
-    ImageButton mpMypage;
+    ImageButton mpBack, mpMypage;
+    Button F5;
+
 
 
     @Override
@@ -22,6 +30,7 @@ public class MainActivity8 extends AppCompatActivity {
         mpBack = findViewById(R.id.ibtn_mpBack);
         mpMypage = findViewById(R.id.ibtn_mpmypage);
 
+        F5 = findViewById(R.id.btn_f5);
         mpBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) { finish();}
@@ -33,5 +42,12 @@ public class MainActivity8 extends AppCompatActivity {
                 startActivity(mpmypage);
             }
         });
+        F5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "새로고침 준비중입니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
